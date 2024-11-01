2c77328ab09c12bb754d03a96769a1482e579d41
jaekim3220 <jehyunkim9@gmail.com>
2024-11-01 23:52:03 +0900

    DB 연동 구현 중 BO를 받아오지 못하는 현상이 발생

--- src/main/java/com/sns/user/UserRestController.java
+++ src/main/java/com/sns/user/UserRestController.java
@@ -3,20 +3,32 @@ package com.sns.user;
 import java.util.HashMap;
 import java.util.Map;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.sns.user.bo.UserBO;
+import com.sns.user.entity.UserEntity;
+
 @RestController
 @RequestMapping("/sns")
 public class UserRestController {
 
+	// 어노테이션(Annotation)
+	@Autowired // DI(Dependency Injection) : 의존성 주입
+	private UserBO userBO;
+	
 	@GetMapping("/is-duplicate-id")
 	public Map<String, Object> isDuplicateId(
 			@RequestParam("loginId") String loginId) { // HTML, JS에서 설정한 파라미터(변수)를 사용
 		
+		// DB SELECT breakpoint 2
+		UserEntity user = userBO.getUserEntityByLoginId(loginId);
+		
 		// 응답 값 breakpoint 1
+		// 응답값 => Map => JSON String
 		// {"code":200, "is_duplicate_id":true}
 		Map<String, Object> result = new HashMap<>();
 		result.put("code", 200);
--- src/main/java/com/sns/user/bo/UserBO.java
+++ src/main/java/com/sns/user/bo/UserBO.java
@@ -1,10 +1,12 @@
 package com.sns.user.bo;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import com.sns.user.entity.UserEntity;
 import com.sns.user.repository.UserRepository;
 
+@Service
 public class UserBO {
 
 	@Autowired
@@ -12,7 +14,7 @@ public class UserBO {
 	
 	// 컨트롤러한테 (아이디 중복 확인 용)
 	// input : loginId
-    // output : UserEntity or null (단건)
+    // output : UserEntity or null (단건일 경우 Optional로 감싸서 구현)
 	public UserEntity getUserEntityByLoginId(String loginId) {
 		return userRepository.findByLoginId(loginId);
 	}
