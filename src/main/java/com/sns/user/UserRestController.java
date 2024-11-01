67d3e588eb8e0e34c0c38a1ff792190d028cd024
jaekim3220 <jehyunkim9@gmail.com>
2024-11-01 21:40:28 +0900

    resetController 생성

---
+++ src/main/java/com/sns/user/UserRestController.java
@@ -0,0 +1,21 @@
+package com.sns.user;
+
+import java.util.Map;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/user")
+public class UserRestController {
+
+	@GetMapping("/is-duplicate-id")
+	public Map<String, Object> isDuplicateId(
+			@RequestParam("loginId") String loginId) {
+		
+		// 응답 값 breakpoint 1
+		return "";
+	}
+}
