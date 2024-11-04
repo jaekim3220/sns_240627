372737635fcb2e873484876352e86bd506ffaa36
jaekim3220 <jehyunkim9@gmail.com>
2024-11-04 23:37:29 +0900

    Controller 영역 설명 추가

--- src/main/java/com/sns/timeline/TimelineController.java
+++ src/main/java/com/sns/timeline/TimelineController.java
@@ -3,6 +3,21 @@ package com.sns.timeline;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
+/*
+DB연동 : View영역 <--> Controller영역(Domain) <--> Service(BO)영역 <--> Repository영역(Mapper) <--> DB영역 
+*/
+
+//View영역
+
+/*
+<Response 방법 - 서버 기준>
+@Controller + return String => ViewResolver => HTML 파일 렌더링(Model) => HTML
+@Controller + @ResponseBody return String => HTTPMessageConverter => HTML
+@Controller + @ResponseBody return 객체(map, list) => HTTPMessageConverter => jackson => JSON
+@RestController return map => JSON
+Model은 HTML일 경우 사용(@ResponseBody일 경우 Model 사용 불가)
+*/
+
 @Controller
 public class TimelineController {
 
