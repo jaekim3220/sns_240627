6bbedaefa7c2ee00b2f4985298d0df6ec2bd5cbb
jaekim3220 <jehyunkim9@gmail.com>
2024-11-04 19:47:13 +0900

    session 정보를 받아와 로그인 화면 구현(로그인 됐을 경우, 안 됐을 경우의 HTML 화면 조정) + SESSION 초기화를 사용한 로그아웃) => /post/post-list-view 구현이 안 되서 404가 맞음 + Timeline 구현

--- src/main/java/com/sns/user/UserController.java
+++ src/main/java/com/sns/user/UserController.java
@@ -4,6 +4,8 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import jakarta.servlet.http.HttpSession;
+
 /*
 @Controller + return String => ViewResolver => HTML 파일 렌더링(Model) => HTML
 @Controller + @ResponseBody return String => HTTPMessageConverter => HTML
@@ -42,6 +44,18 @@ public class UserController {
 		// 가운데 Layout(section) 조각만 내려주면
 		// 전체 Layout과 함께 구성된다
 		return "user/signIn";
+	}
+	
+	
+	@GetMapping("/sign-out")
+	public String signOut(HttpSession session) {
+		// session 내용 초기화
+		session.removeAttribute("userId");
+		session.removeAttribute("userLoginId");
+		session.removeAttribute("userName");
+		
+		// 로그인 페이지로 이동(redirect)
+		return "redirect:/user/sign-in-view";
 	}
 	
 }
