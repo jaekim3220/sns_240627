671ff60bd3b7d5f16ea5dfb2cfb1d64d691a0e4e
jaekim3220 <jehyunkim9@gmail.com>
2024-11-06 00:01:33 +0900

    DB 연동을 위한 BO(Service) 영역 생성

--- src/main/java/com/sns/post/bo/PostBO.java
+++ src/main/java/com/sns/post/bo/PostBO.java
@@ -1,6 +1,12 @@
 package com.sns.post.bo;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import com.sns.post.entity.PostEntity;
+import com.sns.post.repository.PostRepository;
 
 /*
 DB연동 : View영역 <--> Controller영역(Domain) <--> Service(BO)영역 <--> Repository영역(Mapper) <--> DB영역 
@@ -11,4 +17,14 @@ DB연동 : View영역 <--> Controller영역(Domain) <--> Service(BO)영역 <--> 
 @Service // Spring Bean 등록
 public class PostBO {
 
+	
+	@Autowired
+	private PostRepository postRepository;
+	
+	
+	// input:X
+	// output:List<PostEntity>
+	public List<PostEntity> getPostList() {
+		return postRepository.findByOrderByIdDesc(); // 최신 데이터 우선(orderBy)
+	}
 }
