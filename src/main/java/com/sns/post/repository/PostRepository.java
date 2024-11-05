6b68173c21972b12abb4bccf52669887df097ab4
jaekim3220 <jehyunkim9@gmail.com>
2024-11-06 00:02:14 +0900

    DB 연동을 위한 메서드 생성을 위해 Repository 생성

--- src/main/java/com/sns/post/repository/PostRepository.java
+++ src/main/java/com/sns/post/repository/PostRepository.java
@@ -1,9 +1,15 @@
 package com.sns.post.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.sns.post.entity.PostEntity;
 
 public interface PostRepository extends JpaRepository<PostEntity, Integer>{
 
+	// 메서드 생성
+	
+	// @GetMapping("/timeline") 메서드
+	public List<PostEntity> findByOrderByIdDesc();
 }
