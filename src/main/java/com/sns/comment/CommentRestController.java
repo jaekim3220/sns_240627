1404cdd9ad4ec6b16937096c35e2d3ca4cd47e08
jaekim3220 <jehyunkim9@gmail.com>
2024-11-07 23:18:01 +0900

    API 요청 구현 완료 => TODO : HTML에서 요청하는 JS, AJAX 구현

--- src/main/java/com/sns/comment/CommentRestController.java
+++ src/main/java/com/sns/comment/CommentRestController.java
@@ -3,8 +3,6 @@ package com.sns.comment;
 import java.util.HashMap;
 import java.util.Map;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -13,6 +11,7 @@ import org.springframework.web.bind.annotation.RestController;
 import com.sns.comment.bo.CommentBO;
 
 import jakarta.servlet.http.HttpSession;
+import lombok.RequiredArgsConstructor;
 
 /*
 DB연동 : View영역 <--> Controller영역(Domain) <--> Service(BO)영역 <--> Repository영역(Mapper) <--> DB영역 
@@ -28,14 +27,27 @@ DB연동 : View영역 <--> Controller영역(Domain) <--> Service(BO)영역 <--> 
 @RestController return map => JSON
 */
 
+@RequiredArgsConstructor
 @RestController
 @RequestMapping("/comment")
 public class CommentRestController {
 
 	
+	/*
 	// 어노테이션(Annotation)
 	@Autowired // DI(Dependency Injection) : 의존성 주입
 	private CommentBO commentBO;
+	*/
+	
+	/* 생성자를 통한 의존성 주입(트렌드, 권장) */
+//	private final CommentBO commentBO;
+//	public CommentRestController(CommentBO commentBO) {
+//		this.commentBO = commentBO;
+//	}
+	
+	/* 파라미터가 너무 많을 경우 Lombok을 사용해 구현 가능 */
+	private final CommentBO commentBO;
+	
 	
 	// 댓글 쓰기 API
 	@PostMapping("/create")
