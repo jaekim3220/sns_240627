263dcde3c626d6fb47355668c1957bc75aa8aebd
jaekim3220 <jehyunkim9@gmail.com>
2024-11-08 23:31:04 +0900

    DB에서 추출한 데이터를 가공하고 생성할 DTO의 기능을 구현하기 위한 DTO 파일을 생성 => TODO : DTO 파일에 DB에서 추출한 데이터를 할당, 삽입하는 BO 파일 생성

---
+++ src/main/java/com/sns/timeline/domain/CardDTO.java
@@ -0,0 +1,30 @@
+package com.sns.timeline.domain;
+
+import com.sns.post.entity.PostEntity;
+import com.sns.user.entity.UserEntity;
+
+import lombok.Getter;
+import lombok.Setter;
+
+// DTO는 `계층간 데이터 교환을 위해 사용하는 객체`로 데이터 교환이 일어나는 객체 만큼 생성
+// 글 1개 => HTML의 card 1개
+
+// @Data // @Data의 경우 가끔 에러가 발생할 수도 있으므로 Getter, Setter 사용
+@Getter
+@Setter
+public class CardDTO {
+
+	// 글 1개
+	private PostEntity postEntity; // postEntity 전체 추출
+	
+	
+	// 글쓴이
+	private UserEntity userEntity;
+	
+	
+	// 댓글 N개
+	
+	
+	// 좋아요 N개
+	
+}
